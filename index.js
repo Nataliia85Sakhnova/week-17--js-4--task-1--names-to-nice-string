c08cbf246e5fa7a4ca3107335746f054d9f7fd7b
Razhatawka <nataliia85sakhnova@gmail.com>
2022-06-10 20:50:14 +0300

    done

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Сделайте интерфейс преобразования ФИО на три поля (input).
 // Учтите, пожалуйста, все нюансы - лишние пробелы, отсутствие больших букв в именах и пр.
 
-const shortSurname = document.addEventListener("click", document.getElementById("surname").value.trim());
-console.log(surname);
+document.getElementById("names").onchange = (e) => {
+const Arr = document.getElementById("names").value.split(" ")
+const Arr2 = Arr.filter((item) => {
+  return item; // return item.lenght>0; одно и то же
+})
+console.log(Arr2);
 
+const niceLetters = Arr2.map((item) => {
+  return item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase();
+})
+console.log(niceLetters);
 
+console.log(niceLetters.join(" "));
+}
 
-// const niceSurname = surname.charAt(0).toUpperCase + surname[1].toLowerCase(); //первая буква стала заглавной
-
-
-// const arr = [surname, name, patronymic]
